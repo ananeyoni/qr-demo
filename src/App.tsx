76c1a726a2ffc8ba2337c398bc3519c019b11f02
yoni_avramento <yoni@ananey.com>
2022-05-16 18:17:57 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { VideoDecode } from "./components/scanner";
+
+function App() {
+  return (
+    <div className="helloWorld">
+      <h3>Hello World</h3>
+      <div className="container">
+        <VideoDecode/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
